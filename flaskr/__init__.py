3eb8696038f2fbbf9dc8da586747f13031e0837e
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-06-03 18:47:17 +0000

    Add initial project setup with .gitignore, config, and Flask app structure

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,27 @@
-from flask import Flsk
+import os
+from dotenv import load_dotenv
 
-print('hello')
+load_dotenv()
+
+from flask import Flask
+
+def create_app(test_config=None):
+    #Create and config the app
+    app=Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(
+        SECRETE_KEY="dev",
+        DATABASE=os.path.join(app.instance_path,'flask.sqlite'),
+    )
+    if test_config is None:
+        app.config.from_file('.env',load='.env',silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        pass
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    @app.route("/")
+    def hello():
+        return "Hello world"
+    return app
