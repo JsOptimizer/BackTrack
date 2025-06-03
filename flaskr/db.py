26f42c80774e51c2de731b6fc27fb46dfc4b18a9
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-06-03 23:20:04 +0000

    Add functions to manage SQLite database connection in db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,2 +1,20 @@
 import sqlite3
 
+from datetime import datetime
+
+import click
+from flask import current_app ,g
+
+def get_db():
+    if "db" not in g:
+       g.db=sqlite3.connect(
+        current_app.config["DATABASE"],
+        detect_type=sqlite3.PARSE_DECLTYPES
+       )
+       g.db.row_factory=sqlite3.Row
+       pass
+    return g.db
+def close_db(e:None):
+    db=g.pop("db",None)
+    if db is not None:
+        db.close()
