9a052cbe5b2a00b1048a0cb034dd4b4417a06669
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-06-06 23:43:46 +0000

    Add logout functionality and a login_required decorator to the authentication module, enhancing session management and user access control

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -2,7 +2,7 @@ from flask import (g,Blueprint,flash,redirect,render_template,session,url_for,re
 
 from werkzeug.security import (check_password_hash,generate_password_hash)
 from flaskr.db import (get_db)
-
+import functools
 bp=Blueprint('auth',__name__,url_prefix='/auth')
 
 @bp.route("/register",methods=('GET','POST'))
@@ -59,3 +59,15 @@ def load_logged_in_user():
         pass
     else:
         g.user= get_db().execute('SELECT * FROM user WHERE id=?'(user_id,)).fetchone
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+    return wrapped_view
